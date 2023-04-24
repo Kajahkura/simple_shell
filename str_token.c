efbf69723106fbfef8f2adb1fd99b3f57452b66c
philisiwe99 <previouslilly31@gmail.com>
2023-04-24 13:43:07 -0700

    SIMPLE SHELL

--- str_token.c
+++ str_token.c
@@ -7,36 +7,36 @@
 */
 char *_strtok(char *line, char *delim)
 {
-	int j;
-	static char *str;
-	char *copystr;
+int j;
+static char *str;
+char *copystr;
 
-	if (line != NULL)
-		str = line;
-	for (; *str != '\0'; str++)
+if (line != NULL)
+	str = line;
+for (; *str != '\0'; str++)
+{
+	for (j = 0; delim[j] != '\0'; j++)
 	{
-		for (j = 0; delim[j] != '\0'; j++)
-		{
-			if (*str == delim[j])
-			break;
-		}
-		if (delim[j] == '\0')
-			break;
+		if (*str == delim[j])
+		break;
 	}
-	copystr = str;
-	if (*copystr == '\0')
-		return (NULL);
-	for (; *str != '\0'; str++)
+	if (delim[j] == '\0')
+		break;
+}
+copystr = str;
+if (*copystr == '\0')
+	return (NULL);
+for (; *str != '\0'; str++)
+{
+	for (j = 0; delim[j] != '\0'; j++)
 	{
-		for (j = 0; delim[j] != '\0'; j++)
+		if (*str == delim[j])
 		{
-			if (*str == delim[j])
-			{
-				*str = '\0';
-				str++;
-				return (copystr);
-			}
+			*str = '\0';
+			str++;
+			return (copystr);
 		}
 	}
-	return (copystr);
+}
+return (copystr);
 }
